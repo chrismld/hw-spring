94c2db80f2d351f1bc2b9826202cfaf3a5612bbb
Christian Melendez <christianhxc@gmail.com>
2017-11-01 23:12:49 -0600

    Updated to use the actors endpoint

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.core.ParameterizedTypeReference;
 import java.util.Arrays;
-import java.util.List;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,7 +18,13 @@ public class Application {
     @RequestMapping("/")
     public Movie home() {
 
-        Set<Actor> actors = new HashSet<>(Arrays.asList(new Actor("George Lucas")));
+        RestTemplate restTemplate = new RestTemplate();
+        Set<Actor> actors = restTemplate.exchange(
+                    System.getenv("ACTORS_ENDPOINT"),
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<Set<Actor>>(){}
+                ).getBody();
         Set<Writer> writers = new HashSet<>(Arrays.asList(new Writer("George Lucas")));
         Movie movie = new Movie("Star Wars: Episode IV - A New Hope", actors, writers);
 
